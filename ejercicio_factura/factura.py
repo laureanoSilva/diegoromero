6c41e72615fd013108f52a096bda963182f92938
Andres Garagiola <garagiol@ar.ibm.com>
2017-11-13 19:14:43 -0300

    Pasa ejercicio de lista a diccionario.

--- ejercicio_factura/factura.py
+++ ejercicio_factura/factura.py
@@ -3,9 +3,9 @@ Created on 4 nov. 2017
 @author: Diego
 '''
 
-def mostrarProductos(matriz):
-    for a in range(0,len(matriz[0])):
-        print(str(a)+"-> "+matriz[0][a]+" - $"+str(matriz[1][a]))
+def mostrarProductos(diccionario):
+    for a in diccionario:
+        print(a+"-> "+diccionario[a]["nombre"]+" - $"+str(diccionario[a]["precio"]))
 
 
 def mostrarTicket(matriz):
@@ -13,11 +13,16 @@ def mostrarTicket(matriz):
     for a in range(0,len(matriz[0])):
         print(matriz[0][a]," - ",matriz[1][a]," - $",matriz[2][a])
         sumaTotalTicket = sumaTotalTicket + matriz[2][a]
-    print("Total: ",sumaTotalTicket)       
+    print("Total: ",sumaTotalTicket)
 
 
-productos = [["Yerba","Azucar","Leche","Pan"],[70,15,25,26]]
-ticket = [[],[],[]] 
+productos = {}
+productos["A001"]={"nombre":"yerba","precio":75}
+productos["A002"]={"nombre":"azucar","precio":25}
+productos["A003"]={"nombre":"agua","precio":15}
+productos["A004"]={"nombre":"pan","precio":35}
+
+ticket = [[],[],[]]
 
 #debug
 termina = 'no'
@@ -26,9 +31,9 @@ while termina == 'no':
     codigo = input("ingresar codigo de articulo")
     cantidad = input("ingresar cantidad de articulo")
     ticket[0].append(int(cantidad))
-    ticket[1].append(productos[0][int(codigo)])
-    ticket[2].append(int(cantidad)*productos[1][int(codigo)])                                  
+    ticket[1].append(productos[codigo]["nombre"])
+    ticket[2].append(int(cantidad)*productos[codigo]["precio"])
     termina = input("termina? si/no")
 
-   
+
 mostrarTicket(ticket)
